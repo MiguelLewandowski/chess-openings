d3f0b170bc17b584d9aea98989f7839a2ef75a05
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2026-03-25 16:41:57 -0300

    feat: adiciona controle de estado global com zustand

---
+++ src/store/GameStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+interface GameState {
+    hasError: boolean;
+    setHasError: (newHasError: boolean) => void;
+    fen: string;
+    setFen: (newFen: string) => void;
+    comment: string;
+    setComment: (newComment: string) => void;
+}
+
+export const useGameStore = create<GameState>((set) => ({
+    hasError: false,
+    setHasError: (newHasError) => set({ hasError: newHasError }),
+    
+    fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', // Posição inicial por defeito
+    setFen: (newFen) => set({ fen: newFen }),
+    
+    comment: '',
+    setComment: (newComment) => set({ comment: newComment })
+}));
