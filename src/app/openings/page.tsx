3bb396324aebf982ac6f449ce886509798f3bb6b
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2026-03-27 13:50:28 -0300

    feat: adiciona alguns dados das aberturas para testar a lista de aberturas

--- src/app/openings/page.tsx
+++ src/app/openings/page.tsx
@@ -1,5 +1,19 @@
-export default async function OpeningsCatalogPage(){
+import { getAllOpenings } from "@/services/opening.service"
+import Link from "next/link";
+
+export default async function OpeningsCatalogPage() {
+    const openings = await getAllOpenings();
     return <>
-    ''
+        {openings.map((opening) => {
+            return <Link href={`/openings/${opening.slug}`} key={opening.id}>
+                <div className="border p-4 rounded shadow hover:bg-gray-50"> 
+                    <h2>{opening.name}</h2>
+                    <p>{opening.description}</p>
+                    <p>Lições disponíveis: {opening.lessons.length}</p>
+                    <br />
+                </div>
+            </Link>
+        })}
+
     </>
 }
